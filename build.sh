54a134fa9818e256476b4a621524d2d7aa3bb168
Ed Bordin <edbordin@gmail.com>
2020-11-19 17:55:02 +1100

    install more packages needed for build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 ARCH=$1
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential gcc-10 g++-10 gnat-10 wget file
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential gcc-10 g++-10 gnat-10 wget file cpio unzip rsync bc
 
 BUILDROOT_VER=buildroot-2020.08.2
 wget --progress=dot https://buildroot.org/downloads/$BUILDROOT_VER.tar.gz -O buildroot.tar.gz
