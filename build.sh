5f422d8996e90c8d614493db1a026982253dfa03
Ed Bordin <edbordin@gmail.com>
2020-11-19 17:43:07 +1100

    initial attempt at CI for armv7l

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+ARCH=$1
+
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential gcc-10 g++-10 gnat-10 wget
+
+BUILDROOT_VER=buildroot-2020.08.2
+wget --progress=dot https://buildroot.org/downloads/$BUILDROOT_VER.tar.gz -O buildroot.tar.gz
+tar xvf buildroot.tar.gz
+pushd $BUILDROOT_VER
+cp -f ../$ARCH.config .config
+make HOSTCC=gcc-10 HOSTCXX=g++-10 -j`nproc`
+popd
+
+tar -czf buildroot_$ARCH.tar.gz $BUILDROOT_VER/output/host
