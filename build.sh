289167c10bc329962d7face3ab9931f0de7900f7
Ed Bordin <edbordin@gmail.com>
2020-11-19 17:51:49 +1100

    install file package, only make sdk

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -e
 ARCH=$1
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential gcc-10 g++-10 gnat-10 wget
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential gcc-10 g++-10 gnat-10 wget file
 
 BUILDROOT_VER=buildroot-2020.08.2
 wget --progress=dot https://buildroot.org/downloads/$BUILDROOT_VER.tar.gz -O buildroot.tar.gz
 tar xvf buildroot.tar.gz
 pushd $BUILDROOT_VER
 cp -f ../$ARCH.config .config
-make HOSTCC=gcc-10 HOSTCXX=g++-10 -j`nproc`
+make HOSTCC=gcc-10 HOSTCXX=g++-10 -j`nproc` sdk
 popd
 
 tar -czf buildroot_$ARCH.tar.gz $BUILDROOT_VER/output/host
